0c8c9c18c49ba7e0028233486521ff9c1f53b3cb
AbdulRauof Nasouf <a.m.nasouf@gmail.com>
2022-09-04 19:31:45 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Frontend Mentor - Interactive rating component solution
+
+This is a solution to the [Interactive rating component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Select and submit a number rating
+- See the "Thank you" card state after submitting a rating
+
+### Screenshot
+
+![Desktop design](./screenshots/desktop.png)
+![Desktop thank you state](./screenshots/desktop-thank-you.png)
+![Mobile design](./screenshots/mobile.png)
+![Mobile thank you state](./screenshots/mobile-thank-you.png)
+
+### Links
+
+- Solution URL: [https://github.com/ANasouf/fem-interactive-rating-component.git](https://github.com/ANasouf/fem-interactive-rating-component.git)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## Author
+
+- Frontend Mentor - [@ANasouf](https://www.frontendmentor.io/profile/ANasouf)
---
+++ vue-project/src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import IconStar from "@/components/IconStar.vue";
+import { ref } from "vue";
+
+const ratings = ref([1, 2, 3, 4, 5]);
+const rate = ref(0);
+const submitted = ref(false);
+
+const selectRating = (r: number) => (rate.value = r);
+</script>
+<template>
+  <div class="card flex flex-col" :class="{ 'justify-center': submitted }">
+    <template v-if="!submitted">
+      <div class="circle flex align-center">
+        <IconStar class="mx-auto" />
+      </div>
+      <h2>How did we do?</h2>
+      <p>
+        Please let us know how we did with your support request. All feedback is
+        appreciated to help us improve our offering!
+      </p>
+      <div class="flex justify-between">
+        <button
+          v-for="r in ratings"
+          :key="r"
+          class="rating circle"
+          :class="{ 'rating--active': rate === r }"
+          @click="selectRating(r)"
+        >
+          {{ r }}
+        </button>
+      </div>
+      <button class="uppercase btn" @click="submitted = true">Submit</button>
+    </template>
+    <template v-else>
+      <img src="./assets/images/illustration-thank-you.svg" alt="Thank you" />
+      <div class="pill">You selected {{ rate }} out of 5</div>
+      <h2>Thank you!</h2>
+      <p>
+        We appreciate you taking the time to give a rating. If you ever need
+        more support, don’t hesitate to get in touch!
+      </p>
+    </template>
+  </div>
+</template>
